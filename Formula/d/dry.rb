7094993ab31172bd8107703ff72746dc53ebba07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-28 02:51:43 +0000

    dry 0.12.0
    dry: remove `--profile` check
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/dry.rb
+++ Formula/d/dry.rb
@@ -1,8 +1,8 @@
 class Dry < Formula
   desc "Terminal application to manage Docker and Docker Swarm"
   homepage "https://moncho.github.io/dry/"
-  url "https://github.com/moncho/dry/archive/refs/tags/v0.11.2.tar.gz"
-  sha256 "8fdb888f3f0c2298c531d5e23acfc0a55c7e4e881ad7365cc0dbecb8ec6c3b89"
+  url "https://github.com/moncho/dry/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "9d556e385ed35a2015dcd5904daa0751c42ee5ec58df71368b1844245551f771"
   license "MIT"
   head "https://github.com/moncho/dry.git", branch: "master"
 
@@ -31,6 +31,5 @@ class Dry < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/dry --version")
     assert_match "A tool to interact with a Docker Daemon from the terminal", shell_output("#{bin}/dry --description")
-    assert_match "Dry could not start", shell_output("#{bin}/dry --profile 2>&1")
   end
 end
