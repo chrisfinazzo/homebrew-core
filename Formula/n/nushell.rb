b141c360b5077f287a9c97c69f7d45c82d28e7bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-01 03:15:57 +0000

    nushell 0.111.0

--- Formula/n/nushell.rb
+++ Formula/n/nushell.rb
@@ -1,8 +1,8 @@
 class Nushell < Formula
   desc "Modern shell for the GitHub era"
   homepage "https://www.nushell.sh"
-  url "https://github.com/nushell/nushell/archive/refs/tags/0.110.0.tar.gz"
-  sha256 "e4c95f743cea3d985ab90e03fd35707a46eef926d407ed363f994155c1ca5055"
+  url "https://github.com/nushell/nushell/archive/refs/tags/0.111.0.tar.gz"
+  sha256 "e3a7980bb5532016036d9fdbbe0a2acc5a73f9549d1842ff6c8c0de2a6d1ddbe"
   license "MIT"
   head "https://github.com/nushell/nushell.git", branch: "main"
 
